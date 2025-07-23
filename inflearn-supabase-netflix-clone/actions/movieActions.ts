df70d982b47fc3899edcd48e56faa898a2af0ae6
goeun23 <abgoeun3339@gmail.com>
2025-07-23 15:16:15 +0900

    Netflex > Part4. 무한 스크롤 기능 & SEO 구현

--- inflearn-supabase-netflix-clone/actions/movieActions.ts
+++ inflearn-supabase-netflix-clone/actions/movieActions.ts
@@ -9,16 +9,37 @@ function handleError(error) {
   }
 }
 
-export async function searchMovies(search = "") {
+export async function searchMovies({ search, page, pageSize }) {
   const supabase = await createServerSupabaseClient()
 
-  const { data, error } = await supabase
+  const { data, error, count } = await supabase
     .from("movie")
     .select("*")
     .like("title", `%${search}%`)
+    .range((page - 1) * pageSize, page * pageSize - 1)
+  // limit(0, 1000) 과 같은 식
 
-  handleError(error)
-  return data
+  // hasNextPage는 직접 계산해줘야하는데, 여기서 suapbase에서 count를 활용
+  // count : 전체 쿼리의 갯수
+  const hasNextPage = count > page * pageSize
+
+  if (error) {
+    console.error(error)
+    return {
+      data: [],
+      count: 0,
+      page: null,
+      pageSize: null,
+      error,
+    }
+  }
+
+  return {
+    data,
+    page,
+    pageSize,
+    hasNextPage,
+  }
 }
 
 export async function getMovie(id) {
