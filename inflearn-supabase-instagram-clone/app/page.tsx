8b70a00e103305c444d68603c7fc845a1e0740b2
goeun23 <abgoeun3339@gmail.com>
2025-07-24 09:50:34 +0900

    Instagram > Part 4. 인증링크 방식의 회원가입

--- inflearn-supabase-instagram-clone/app/page.tsx
+++ inflearn-supabase-instagram-clone/app/page.tsx
@@ -1,13 +1,20 @@
 import LoginButton from "./LoginButton"
+import { createServerSupabaseClient } from "utils/supabase/server"
 
 export const metadata = {
   title: "Inflearngram",
   description: "Instagram Clone Project",
 }
-export default function Home() {
+export default async function Home() {
+  const supabase = createServerSupabaseClient()
+  const {
+    data: { session },
+  } = await (await supabase).auth.getSession()
   return (
     <main className="w-full h-screen flex flex-col gap-2 items-center justify-center">
-      <h1 className="font-bold text-xl">Welcome{"gouen Choi"}</h1>
+      <h1 className="font-bold text-xl">
+        Welcome {session?.user?.email?.split("@")?.[0]}
+      </h1>
       <LoginButton />
     </main>
   )
